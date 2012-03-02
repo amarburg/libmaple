03c83ce421b4dae3d6f85bb7a6619559a3f43afb
Aaron Marburg <aaron.marburg@pg.canterbury.ac.nz>
2012-03-02 20:06:10 +1300

    Added #define for disabling Wirish HardwareSerial Serial3, and #warnings when Serial2 or Serial3 are disabled.

--- wirish/comm/HardwareSerial.h
+++ wirish/comm/HardwareSerial.h
@@ -79,9 +79,16 @@ extern HardwareSerial Serial1;
 
 #ifndef DISABLE_SERIAL2
 extern HardwareSerial Serial2;
+#else
+#warning "Disabling Serial2 in HardwareSerial.h"
 #endif
 
+#ifndef DISABLE_SERIAL3
 extern HardwareSerial Serial3;
+#else
+#warning "Disabling Serial3 in HardwareSerial.h"
+#endif
+
 #if defined(STM32_HIGH_DENSITY) && !defined(BOARD_maple_RET6)
 extern HardwareSerial Serial4;
 extern HardwareSerial Serial5;
